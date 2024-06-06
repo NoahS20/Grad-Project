3ef5c8a1e6548bd432040f239c883221bbeaf70b
NoahS20 <noahgoes50@gmail.com>
2024-06-05 22:52:18 -0400

    Missed backend changes. Adding this too

--- Back End/pythonflask.py
+++ Back End/pythonflask.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
@@ -9,9 +11,24 @@ def get_data():
 
 @app.route('/api/data', methods=['POST'])
 def post_data():
-    data = request.json
-    response = {"received_data": data}
-    return jsonify(response)
+    if request.is_json:
+        data = request.get_json()
+        response = {"received_data": data}
+        return jsonify(response)
+    else:
+        return jsonify({"error": "Invalid content type, must be application/json"}), 400
+
+@app.route('/api/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    return jsonify({"message": "File uploaded successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+ 
