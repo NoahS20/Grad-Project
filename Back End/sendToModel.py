42dbc9973bbe28e14679c821c5aa92ce1cf91ec6
NoahS20 <noahgoes50@gmail.com>
2024-07-19 19:10:41 -0400

    Added validation that checks to make sure question is loaded and returns an error message if not

--- Back End/sendToModel.py
+++ Back End/sendToModel.py
@@ -6,25 +6,31 @@ import time
 import os
 
 def sendFile_and_ChatGPTResponse(file_content):
-    questions = readtxt('.\Temp_Save\questions.txt')
-    AIresponse = getAIResponse.send_request(questions)
+    questions = readtxt('.\Temp_Save\questionsFORAI129.txt')
+    if(questions == ""):
+        return "Please upload question before sending answers."
+    print(questions)
+    #AIresponse = getAIResponse.send_request(questions)
     return "Send to model file success"
 
 def sendTypedResponse_and_ChatGPTResponse(student_answer):
-    questions = readtxt('.\Temp_Save\questions.txt')
-    AIresponse = getAIResponse.send_request(questions)
+    questions = readtxt('.\Temp_Save\questionsFORAI129.txt')
+    if(questions == ""):
+        return "Please upload question before sending answers."
+    print(questions)
+    #AIresponse = getAIResponse.send_request(questions)
     return "Send to model typed success"
 
 
 def readtxt(file_path):
-    print(os.path.isfile(file_path))
+    #print(os.path.isfile(file_path))
     contents = ""
-    checkIfDOne(file_path)
+    #checkIfDOne(file_path)
     try:
         with open(file_path, 'r') as file:
             for line in file:
                 contents = contents + line.rstrip('\n')
-            print(contents)
+            #print(contents)
             return contents
     except FileNotFoundError:
         return f"Error: The file at {file_path} was not found."
